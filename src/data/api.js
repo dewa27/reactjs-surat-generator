7500b97006a0f4f50feaae21f691be905365797e
dewa27 <dewakrishnaaa22@gmail.com>
2023-06-07 11:52:04 +0800

    update api link

--- src/data/api.js
+++ src/data/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = `${process.env.API_URL}/api`;
+const baseURL = `${import.meta.env.VITE_API_URL}/api`;
 
 const client = axios.create({
   baseURL: baseURL,
